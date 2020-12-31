b617c003ca6bdc6546a01d7bf275a14bf0098716
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-31 13:46:46 -0500

    Passed Pokemon prop to inherit list within component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import PokemonList from "./PokemonList";
 
 const App = () => {
   // initial state is set to have Charmander. using array destructuring, we set pokemon as our state and setPokemon as how we'll update state.
   const [pokemon, setPokemon] = useState(["charmander"])
   return (
-    null
+    <PokemonList pokemon={pokemon} />
   );
 }
 
