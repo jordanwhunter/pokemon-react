49267c19bf59b8bfc74d515a711d6dd72db1e19c
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-31 13:53:01 -0500

    Outputting state onto page

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const PokemonList = () => {
+const PokemonList = ({ pokemon }) => {
   return (
     <div>
-      
+      {pokemon.map(p => (
+        // in React, a unique key on the top level element is needed for every single thing inside an array loop. in this instance, we're setting the key to be the name of the pokemon.
+        <div key={p}>
+          {p}
+        </div>
+      ))}
     </div>
   )
 }
