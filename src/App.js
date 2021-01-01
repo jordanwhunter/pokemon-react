223863c699234529d5b12f40adcc4d8f325f93c7
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-01 17:04:11 -0500

    Imported axios and established initial connection with PokéAPI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 
 const App = () => {
-  // initial state is set to have Charmander. using array destructuring, we set pokemon as our state and setPokemon as how we'll update state.
-  const [pokemon, setPokemon] = useState(["charmander"])
+  // initial state is set to empty, so an empty array is needed. using array destructuring, we set pokemon as our state and setPokemon as how we'll update state.
+  const [pokemon, setPokemon] = useState([])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+
   return (
     <PokemonList pokemon={pokemon} />
   );
