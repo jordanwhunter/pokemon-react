a2a7e3c5e29cc16d125bd9adfd161f8a234bc7c3
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-01 17:37:59 -0500

    Implemented cancelToken within axios call to avoid any race conditions in the useEffect hook

--- src/App.js
+++ src/App.js
@@ -11,16 +11,25 @@ const App = () => {
   const [loading, setLoading] = useState(true)
   
   useEffect(() => {
-    axios.get(currentPageUrl).then(res => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      // to avoid a race condition (in the event the user calls multiple times before request completes), we need to use a cancelToken so requests are not overridden. cancelToken is built into axios.
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
       // change loading setting to false since initial loading setting is true - telling the app that the page needs to be loaded
       setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
+
+    return () => cancel()
+
   // every time currentPageUrl changes, this will trigger the useEffect hook and refresh the application
   }, [currentPageUrl])
 
+  if (loading) return "Loading Your Pokémon..."
 
   return (
     <PokemonList pokemon={pokemon} />
