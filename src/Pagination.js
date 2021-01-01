37bff46c80ded163e070605a8cd1cbb165fb0f4b
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-01 18:00:15 -0500

    Added IF gates to tell application not to display buttons if there isn't previous or next list to view - will update with a ternary statement within App.js

--- src/Pagination.js
+++ src/Pagination.js
@@ -3,8 +3,9 @@ import React from "react";
 const Pagination = ({ goToNextPage, goToPrevPage }) => {
   return (
     <div>
-      <button onClick={goToPrevPage}>Previous</button>
-      <button onClick={goToNextPage}>Next</button>
+      {/* this method is another way in which you can perform an IF gate in React: put the variable you want to check in front and then render everything after the ampersands. The second section will never be run if the first section results false */}
+      {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+      {goToNextPage && <button onClick={goToNextPage}>Next</button>}
     </div>
   )
 }
